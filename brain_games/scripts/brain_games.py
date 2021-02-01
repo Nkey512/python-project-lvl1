49b782cdae53dfcfa93b60e9b97ba21046ea18be
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-01 20:18:00 +0300

    Rewrite script

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 def main():
     from brain_games import cli
-    print('Welcome to the Brain Games!')
-    print('Hello, {}!'.format(cli.welcome_user()))
+    cli.welcome_user()
 
 
 if __name__ == '__main__':
