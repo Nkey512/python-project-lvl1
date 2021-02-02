eccdbbb1d62cbf45f9d54793ecc1ee1e3d61a10a
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-02 21:30:19 +0300

    Update pathes to packages

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 def main():
-    from brain_games import cli,\
-        game_structure as gs,\
-        brain_calc_logic as bcl,\
-        levels
+    from brain_games import cli, game_structure as gs, levels
+    from brain_games.games_logic import brain_calc_logic as bcl
 
     user = cli.welcome_user()
     bcl.calc_description()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 def main():
-    from brain_games import cli,\
-        game_structure as gs,\
-        brain_even_logic as bel,\
-        levels
+    from brain_games import cli, game_structure as gs, levels
+    from brain_games.games_logic import brain_even_logic as bel
 
     user = cli.welcome_user()
     bel.even_description()
