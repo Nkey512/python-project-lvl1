376a2cb242b2fd4c09bb65c7cf0f971d560a5db2
Nikita Kuznetsov <nikita.kuznetsov512@gmail.com>
2021-02-03 21:13:48 +0300

    Fix typo

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,5 +4,5 @@ import prompt
 def welcome_user():
     print('Welcome to the Brain Games!')
     user = prompt.string('May I have your name? ')
-    print('Hello {}!'.format(user))
+    print('Hello, {}!'.format(user))
     return user
